fc5cbbfc399e98d5ae9dae55e695ec9b88318e27
misha1290 <eremenko2006qwertyuiop@gmail.com>
2023-12-10 18:26:34 +0200

    1

---
+++ script.js
@@ -0,0 +1,16 @@
+let a5 = document.getElementById("button")
+let a6 = document.getElementById("input")
+a5.addEventListener("click", function () {
+  a5.textContent = a6.value
+});
+
+let a3 = document.querySelector(".picter")
+a3.src = "https://era-mebeli.ru/userfiles/photo/large/2/14203_zvezdnaya-pyl-biryuza.jpg"
+
+let a1 = document.querySelector(".image")
+let a2 = document.querySelector(".link")
+a1.alt = "New description"
+a2.href = "https://meet.google.com/bgs-ybxo-mab"
+
+let a4 = document.querySelector(".li")
+a4.textContent = "Новий текст для першого елемента"
